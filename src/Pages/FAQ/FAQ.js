40cae908957ed7aa3720ba98a01d485e4290119b
parthasuman <parthasuman123@gmail.com>
2022-10-27 18:23:53 +0600

    faq added

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 const FAQ = () => {
-  return <div>This is FAQ page</div>;
+  return (
+    <div>
+      <Container>
+        1. What is dream learn all about?<br></br>
+        It is online base web learning platfrom.<br></br>
+        Why you use dream learn platfrom? <br></br>
+        To learn here to make your future perfect.
+      </Container>
+    </div>
+  );
 };
 
 export default FAQ;
